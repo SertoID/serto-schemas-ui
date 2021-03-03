98c7a4de50d2945a5cabd5ddb7178e21359488bb
Toby Fox <tobyfox@gmail.com>
2021-03-03 08:44:15 -0700

    feat: Enable (mock for now) schema saving

--- src/context/SertoUiWrapper.tsx
+++ src/context/SertoUiWrapper.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { SertoUiProvider, SertoUiContextInterface } from "serto-ui";
+import { SertoUiProvider, SertoUiContextInterface, createMockApiRequest } from "serto-ui";
 
 import { TrustAgencyContext } from "./TrustAgentProvider";
 import { TrustAgencyService } from "../services/TrustAgencyService";
 import { config } from "../config";
+import { useAuth } from "../services/useAuth";
 
 export interface SertoUiWrapperProps {
   navItems: SertoUiContextInterface["navItems"];
@@ -11,6 +12,7 @@ export interface SertoUiWrapperProps {
 
 export const SertoUiWrapper: React.FunctionComponent<SertoUiWrapperProps> = (props) => {
   const TrustAgent = React.useContext<TrustAgencyService>(TrustAgencyContext);
+  const { isAuthenticated } = useAuth();
 
   const sertoUiContext = {
     navItems: props.navItems,
@@ -20,6 +22,12 @@ export const SertoUiWrapper: React.FunctionComponent<SertoUiWrapperProps> = (pro
     // @TODO/tobek Versioned schema URLs are broken in API, so leaving this commented out until that's fixed:
     // buildSchemaUrl: (slug: string, type: string, version?: string) => `${config.API_URL}/v1/schemas/public/${slug}${version && ("/" + version)}/${type}.json`,
     buildSchemaUrl: (slug: string, type: string) => `${config.API_URL}/v1/schemas/public/${slug}/${type}.json`,
+
+    ...(isAuthenticated && {
+      // @TODO/tobek These aren't implemented in the API yet so just mock for now
+      saveSchema: createMockApiRequest(),
+      unsaveSchema: createMockApiRequest(),
+    }),
   };
 
   return <SertoUiProvider context={sertoUiContext}>{props.children}</SertoUiProvider>;
