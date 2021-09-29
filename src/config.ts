e63dabba1eaad8f61d8e8fd7e8ff906c62b39160
Toby Fox <tobyfox@gmail.com>
2021-09-28 22:41:03 -0400

    chore: Use new consolidated environment domains config

--- src/config.ts
+++ src/config.ts
@@ -1,34 +1,16 @@
-export interface ConfigType {
-  ENVIRONMENT: string;
-  API_URL: string;
-  UI_URL: string;
+import { config as sertoUiConfig, SertoUiConfig, mergeServerConfig } from "serto-ui";
+
+export interface ConfigType extends SertoUiConfig {
   AUTH0_CLIENT_ID: string;
   AUTH0_DOMAIN: string;
-  FEATURE_FLAGS?: string;
 }
 
 const defaultConfig: ConfigType = {
-  ENVIRONMENT: process.env.NODE_ENV || "development",
-  // API_URL: "https://beta.api.schemas.serto.id",
-  API_URL: "https://staging.api.schemas.serto.id",
-  // API_URL: "http://localhost:8083",
-  UI_URL: window.location.origin,
+  ...sertoUiConfig,
+  // SCHEMAS_API_URL: "http://localhost:8083",
+  SCHEMAS_UI_URL: window.location.origin,
   AUTH0_CLIENT_ID: "WRVmn2NOJSGpBFTIF7cJFIkZBMZ6j9Cc",
   AUTH0_DOMAIN: "auth.serto.id",
-  FEATURE_FLAGS: process.env.REACT_APP_FEATURE_FLAGS,
 };
 
-const serverConfigString = (window as any).SERVER_CONFIG;
-let serverConfig: ConfigType | undefined;
-if (serverConfigString && serverConfigString !== "$ENVIRONMENT") {
-  try {
-    serverConfig = JSON.parse(serverConfigString);
-  } catch (e) {
-    console.error("error parsing server config: ", { serverConfigString, defaultConfig, e });
-  }
-}
-
-const config: ConfigType = { ...defaultConfig, ...serverConfig };
-console.log("configuration loaded", { config, defaultConfig, serverConfig });
-
-export { config };
+export const config = mergeServerConfig(defaultConfig);
