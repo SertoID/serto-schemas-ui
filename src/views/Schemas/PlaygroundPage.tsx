b4822080d24e523f45fe30dca25e70c0102b6de6
Toby Fox <tobyfox@gmail.com>
2021-12-15 15:02:21 +0000

    chore: Update copy to reflect new format stuff

--- src/views/Schemas/PlaygroundPage.tsx
+++ src/views/Schemas/PlaygroundPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link as ReactLink } from "react-router-dom";
+import { Link as ReactLink, generatePath } from "react-router-dom";
 import { Box, Text, Link } from "rimble-ui";
 import { H3, colors, baseColors, SchemaPlayground } from "serto-ui";
 import { routes } from "../../constants";
@@ -10,30 +10,30 @@ export const PlaygroundPage: React.FunctionComponent = () => {
     <GlobalLayout url={routes.PLAYGROUND} fullWidth={true}>
       <Box bg={colors.darkGray} color={baseColors.white} px={5} py={4}>
         <H3 m={0} mb={2}>
-          JSON-LD Context Plus Schema Playground
+          Schema Playground
         </H3>
         <Text maxWidth="1150px">
-          Enter{" "}
+          Enter JSON Schema on the left,{" "}
           <Link
-            href="https://docs.google.com/document/d/1l41XsI1nTCxx3T6IpAV59UkBrMfRzC89TZRPYiDjOC4/edit?usp=sharing"
+            href="https://github.com/w3c-ccg/traceability-vocab#ontology-structure"
             target="_blank"
             fontSize={2}
             color={colors.primary.light}
           >
-            JSON-LD Context Plus schema
+            including embedded JSON-LD data
           </Link>{" "}
-          in the top-left quadrant, and the compiled JSON-LD Context and JSON Schema will automatically be generated in
-          the bottom quadrants. You may enter a Verified Credential in the top-right quadrant to validate it against
-          what you have entered.
+          via <code>$comment</code> or <code>$linkedData</code> properties (see examples). The compiled JSON-LD Context
+          will automatically be generated in the bottom-right quadrant. You may enter a Verified Credential in the
+          top-right quadrant to validate it against what you have entered.
         </Text>
         <Text mt={2}>
           See the{" "}
-          <ReactLink to={routes.EDITOR} style={{ textDecoration: "none" }}>
+          <ReactLink to={generatePath(routes.EDITOR)} style={{ textDecoration: "none" }}>
             <Link fontSize={2} color={colors.primary.light}>
               VC Schema editor
             </Link>
           </ReactLink>{" "}
-          for a VC-specific wizard UI and formatted views of schemas using JSON-LD Context Plus.
+          for a VC-specific wizard UI and formatted views of schemas.
         </Text>
       </Box>
 
